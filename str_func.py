38ed93b0ef5e629908bb207ad1381451b529ac9c
Артем Никонов <player1997@mail.ru>
2023-03-08 12:50:46 +0300

    add str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def caps(string):
     return string.capitalize()
+
+
+"""
+Принимает строку, делает все буквы заглавными
+"""
+
+
+def title(string):
+    return string.title()
+
+
+"""""
+Принимает строку, делает первые буквы слов заглавными
+"""""
